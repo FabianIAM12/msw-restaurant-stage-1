9d0ab5f4523f8bb1b10a4b31db414859f5dc8283
FabianIAM12 <fabian.b89@gmail.com>
2019-01-18 16:09:34 +0100

    fixed service worker

--- client/sw.js
+++ client/sw.js
@@ -1,8 +1,10 @@
 var staticCacheName = 'restaurant-static-v1';
+
 self.addEventListener('install', function(event) {
     event.waitUntil(
         caches.open(staticCacheName).then(function(cache) {
             return cache.addAll([
+                '/',
                 '/img/1.jpg',
                 '/img/2.jpg',
                 '/img/3.jpg',
@@ -26,8 +28,7 @@ self.addEventListener('activate', function(event) {
         caches.keys().then(function(cacheNames) {
             return Promise.all(
                 cacheNames.filter(function(cacheName) {
-                    return cacheName.startsWith('restaurant-') &&
-                        cacheName != staticCacheName;
+                    return cacheName.startsWith('restaurant-') && cacheName != staticCacheName;
                 }).map(function(cacheName) {
                     return caches.delete(cacheName);
                 })
